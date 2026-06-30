be4486f3353c6abe2d31aa00549dd64f0605dbe5
Thomas Sierra <tsierra6@gatech.edu>
2026-06-30 00:06:54 -0500

    Finally got collision working after a break

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
-from logger import log_state
+import sys
+from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,14 +12,32 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     while 1 != 0:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
-    
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
